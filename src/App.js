590c418a5d7165cefebf2238a81e35753e1d1c6d
Vinayaka Srinivas <vinny.sri@berkeley.edu>
2022-11-21 22:44:12 -0800

    Basic website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import bb_logo from './BB-Logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={bb_logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BlockBase is a fractional real estate marketplace that lowers the barrier of entry <br></br>
+          for investors in real estate while offering greater liquidity and opportunity to real estate vendors.
         </p>
-        <a
+        <p> This website is still under development. </p>
+        <p>
+          Fill out<tab> </tab>
+          <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://forms.gle/s5UMhGDtyoTdzNU87"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this form
         </a>
+           <tab> </tab>for the most current updates and to be added to our shortlist.
+        </p>
+        
       </header>
     </div>
   );
